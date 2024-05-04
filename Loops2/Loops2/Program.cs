8a7fff8617b12c1708a6b525d0507d150d8ed78c
Khairnar <cxk210037@utdallas.edu>
2024-05-04 17:37:46 -0500

    added loops2

---
+++ Loops2/Loops2/Program.cs
@@ -0,0 +1,23 @@
+﻿string secret_code = "13579";
+string input_code = "00000";
+int tries = 5;
+Console.WriteLine("");
+
+do
+{
+    Console.WriteLine($"Enter a code to open the lock inital code {input_code}");
+    input_code = Console.ReadLine();
+
+    if (input_code == secret_code)
+    {
+        Console.WriteLine("Welcome Home");
+        break;
+    }
+    tries--;
+    Console.WriteLine($"You entered wrong code, {tries} tries left");
+
+}
+while ((tries > 0) && input_code != secret_code);
+
+if (tries == 0)
+    Console.WriteLine("Too many tries you are locked");
